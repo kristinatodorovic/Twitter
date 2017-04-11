2b6c4b8bf76a912daf675206f7468cb6f5f71e7b
Kristina <kristinatodorovic108@gmail.com>
2017-04-11 16:58:47 +0200

    Dodata dokumentacija u klasi Twitter.

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -1,15 +1,35 @@
-
 package com.twitter;
 
 import java.util.LinkedList;
 import com.twitter.poruke.TwitterPoruka;
 
+	/**
+	 * @author Kristina Todorovic
+	 * @version 1.0
+	 */
+
 public class Twitter {
+	
+	/**
+	 * Poruke u vidu liste.
+	 */
+
 	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
+	
+	/**
+	 * Metoda vraca sve poruke kao listu.
+	 * @return lista poruka
+	 */
 
 	public LinkedList<TwitterPoruka> vratiSvePoruke() {
 		return poruke;
 	}
+	
+	/**
+	 * Metoda unosi novu poruku u listu.
+	 * @param korisnik kao string
+	 * @param poruka kao string
+	 */
 
 	public void unesi(String korisnik, String poruka) {
 		// Pravi se nova poruka i puni podacima.
@@ -19,6 +39,19 @@ public class Twitter {
 		// Poruka se unosi u listu na kraj
 		poruke.addLast(tp);
 	}
+	
+	/**
+	 * Metoda vraca niz poruka koje sadrze tag.
+	 * @param maxBroj ceo broj koji predstavlja kapacitet niza
+	 * @param tag string koji proveravamo da li se sadrzi u porukama
+	 * @throws java.lang.RuntimeException ako je unet tag:
+	 * <ul>
+	 * <li>null</li>
+	 * <li>prazan string</li>
+	 * </ul>
+	 * @return niz poruka koje sadrze trazeni tag
+	 * 
+	 */
 
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 		if (tag == null || tag.isEmpty())
@@ -45,3 +78,4 @@ public class Twitter {
 		return rezultat;
 	}
 }
+
